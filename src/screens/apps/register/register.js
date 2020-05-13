673ed994c0ab21636ae28f07746cea66e834a83e
WI HERMES <wihermes@WIs-Device.local>
2020-05-12 19:14:35 -0500

    connect firebase

--- src/screens/apps/register/register.js
+++ src/screens/apps/register/register.js
@@ -1,18 +1,43 @@
 import React from 'react'
 import styles from './styles'
 
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image, SafeAreaView } from 'react-native'
+
+import google from 'assets/img/GoogleR.png'
+import facebook from 'assets/img/FaceR.png'
+import Frontal from 'assets/img/Frontal.png'
+
+//import CheckBox from 'components/shared/checkBox/checkBox'
 
 
 const Register = () => {
 
 
   return (
-    <View>
-      <Text>
-        REGISTER
-      </Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.textH1}>Unete a la comunidad LUROG</Text>
+
+        <View style={styles.containerImg}>
+          <TouchableOpacity >
+            <Image source={facebook} style={styles.img} />
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Image source={google} style={styles.img} />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.contentProfile}>
+          <TouchableOpacity style={styles.contentImg}>
+            <Image source={Frontal} />
+            <Text style={styles.text} >Foto de perfil</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 
 }
--- src/screens/apps/register/styles.js
+++ src/screens/apps/register/styles.js
@@ -1,8 +1,53 @@
+
 import { StyleSheet } from 'react-native'
+
+import { COLOR_LETRA_INICIO } from 'utils/colors';
 
 import normalize from 'react-native-normalize'
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
+  },
+  content: {
+    position: 'relative',
+    top: normalize(30)
+  },
+  containerImg: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  img: {
+    width: normalize(180),
+    height: normalize(45),
+    padding: normalize(20),
+    margin: normalize(4)
+  },
+  textH1: {
+    fontFamily: 'Roboto-Bold',
+    color: COLOR_LETRA_INICIO,
+    fontSize: normalize(18),
+    marginLeft: normalize(20),
+    paddingBottom: normalize(15)
+  },
+
+  contentProfile: {
+    paddingHorizontal: normalize(138),
+    paddingVertical: normalize(40)
+  },
+
+  contentImg: {
+    paddingBottom: normalize(10)
+  },
+
+  text: {
+    fontFamily: 'Roboto-Bold',
+    color: COLOR_LETRA_INICIO,
+    fontSize: normalize(16)
+  }
 })
 
 export default styles
+
